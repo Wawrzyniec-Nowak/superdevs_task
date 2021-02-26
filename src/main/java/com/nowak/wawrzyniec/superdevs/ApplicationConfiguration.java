d7b5c721493337284290a263f10c86259a5f79bb
Wawrzyniec Nowak <nowak.wawrzyniec@gmail.com>
2021-02-26 15:07:48 +0100

    Initial structure with pom, spring boot and apache spark dependencies

---
+++ src/main/java/com/nowak/wawrzyniec/superdevs/ApplicationConfiguration.java
@@ -0,0 +1,35 @@
+package com.nowak.wawrzyniec.superdevs;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    @Bean
+    public SparkConf sparkConf() {
+        SparkConf sparkConf = new SparkConf()
+                .setAppName(appName)
+                .setSparkHome(sparkHome)
+                .setMaster(masterUri);
+
+        return sparkConf;
+    }
+
+    @Bean
+    public JavaSparkContext javaSparkContext() {
+        return new JavaSparkContext(sparkConf());
+    }
+
+    @Bean
+    public SparkSession sparkSession() {
+        return SparkSession
+                .builder()
+                .sparkContext(javaSparkContext().sc())
+                .appName("Java Spark SQL basic example")
+                .getOrCreate();
+    }
+}
